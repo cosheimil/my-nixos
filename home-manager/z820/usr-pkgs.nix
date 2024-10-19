4777d68aa7d088304ae686f7336c49f9f8287bbe
Cosheimil <a.varvus@ya.ru>
2024-10-20 00:24:22 +0300

    hp z820

---
+++ home-manager/z820/usr-pkgs.nix
@@ -0,0 +1,54 @@
+{ pkgs, inputs, ... }: {
+  home.packages = with pkgs; [
+    (pkgs.python311.withPackages (python-pkgs: [
+      python-pkgs.numpy
+      python-pkgs.jupyter
+      python-pkgs.matplotlib
+      python-pkgs.sympy
+      python-pkgs.scipy
+      python-pkgs.pandas
+      python-pkgs.shapely
+      python-pkgs.faker
+      python-pkgs.tqdm
+     ]))
+    # uv
+    devenv
+
+    typst
+    typst-lsp
+    flameshot
+    pandoc
+    
+    pcmanfm
+    pavucontrol
+    gimp
+    btop
+    onlyoffice-bin
+    obsidian
+    lutris
+    okular
+    planify
+    
+    xclip
+    networkmanagerapplet
+    distrobox
+    texlive.combined.scheme-full
+
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" "FantasqueSansMono"]; })
+    jetbrains-mono
+    noto-fonts
+    noto-fonts-emoji
+    openmoji-color
+    twemoji-color-font
+    font-awesome
+    powerline-fonts
+    powerline-symbols
+    noto-fonts-cjk-sans
+    comic-mono
+    comic-relief
+    overpass
+
+    revolt-desktop
+    teamspeak5_client
+  ];
+}
